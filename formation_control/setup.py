c974738bfb7548a1aa2d893be2f816c59cb03508
Nevil Sandaradura <nevil.sandaradura@gmail.com>
2023-04-26 11:22:12 +0300

    first commit

---
+++ formation_control/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+package_name = 'formation_control'
+
+data_files=[]
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append(('share/' + package_name, ['package.xml']))
+data_files.append(('share/' + package_name + '/launch', ['launch/test_launch.py']))
+data_files.append(('share/' + package_name + '/resource', ['resource/mavic_webots.urdf']))
+data_files.append(('share/' + package_name + '/worlds', ['worlds/test_world.wbt']))
+
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=data_files,
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='nevil',
+    maintainer_email='nevil.sandaradura@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'mavic_driver = formation_control.mavic_driver:main'
+        ],
+    },
+)
